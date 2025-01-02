cb9533d1e3184d8f8cd930d3ab2d28a150d1fc71
Freddy <Freddymazmurhutabarat@gmail.com>
2025-01-02 13:17:29 +0700

    initial commit

---
+++ Help Desk SAG/include/ost-config.php
@@ -0,0 +1,10 @@
+<?php
+define('DBTYPE', 'mysql');
+define('DBHOST', 'localhost');
+define('DBNAME', 'osticket_db'); // Nama database
+define('DBUSER', 'osticket_user'); // User database
+define('DBPASS', '1234'); // Password database
+
+define('TABLE_PREFIX', 'ost_'); // Default prefix
+define('ADMIN_EMAIL', 'admin@osticket.freddypmsag.my.id'); // Ganti dengan email admin Anda
+
